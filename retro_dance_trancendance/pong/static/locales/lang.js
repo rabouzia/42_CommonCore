18c8d080aa80d115df3cadda2ffb023760529d24
Ramzy Zoldik <rabouzia@student.42.fr>
2025-03-31 05:21:24 +0200

    added trancedance

---
+++ retro_dance_trancendance/pong/static/locales/lang.js
@@ -0,0 +1,158 @@
+window.lang = {
+	en: {
+		flag : "🇬🇧",
+		homeTitle: "PONG WORLD",
+		subTitle: "Transcendence Edition",
+		modeTitle: "GAME MODE",
+
+		pspeed_title: "PADDLE SPEED",
+		pspeed_lvl1: "Painful",
+		pspeed_lvl2: "Slow",
+		pspeed_lvl3: "Normal",
+		pspeed_lvl4: "Quick",
+		pspeed_lvl5: "Flash",
+		
+		pheight_title: "PADDLE HEIGHT",
+		pheight_lvl1: "Crazy",
+		pheight_lvl2: "Small",
+		pheight_lvl3: "Normal",
+		pheight_lvl4: "Big",
+		pheight_lvl5: "Why?",
+
+		ballspeed_title: "BALL SPEED",
+		ballspeed_lvl1: "Boring",
+		ballspeed_lvl2: "Slow",
+		ballspeed_lvl3: "Normal",
+		ballspeed_lvl4: "Quick",
+		ballspeed_lvl5: "Lightning",
+	
+		ballsize_title: "BALL SIZE",
+		ballsize_lvl1: "Quantic", 
+		ballsize_lvl2: "Small",
+		ballsize_lvl3: "Normal",
+		ballsize_lvl4: "Big",
+		ballsize_lvl5: "Cosmic",
+		player1: "Player 1",
+		player2: "Player 2",
+		player3: "Player 3",
+		
+		card1 :"WELCOME TO PONG'S VIRTUAL MUSEUM...",
+		card2 :"ATARI'S PONG MACHINE WAS RELEASED IN 1972",
+		card3 :"IT WAS THE FIRST<br>ARCADE VIDEO GAME",
+		card4 :"TO BE A HUGE<br>COMERCIAL SUCESS...",
+		card5 :"THE NEXT 10 YEARS WOULD BE KNOWN AS",
+		card6 :"THE GOLDEN AGE OF<br>ARCADE VIDEO GAMES",
+		card7 :"BEFORE THE VIDEO GAME CRASH OF 1983",
+		card8 :"WHEN GAME CONSOLES<br>AND HOME COMPUTERS",
+		card9 :"TOOK OVER AND ARCADES EMPTIED",
+		card10 :"AND POOR PONG WAS<br>NEVER SEEN AGAIN...",
+		card11 :"UNTIL TODAY...<br>LOGIN TO PLAY!",
+
+		win: "Wins"
+	},
+	
+	fr: {
+		flag : "🇫🇷",
+
+		homeTitle: "MONDE DE PONG",
+		subTitle: "Édition Transcendence",
+		modeTitle: "MODE DE JEU",
+		
+		pspeed_title: "VITESSE PLANCHE",
+		pspeed_lvl1: "Douloureux",
+		pspeed_lvl2: "Lent",
+		pspeed_lvl3: "Normal",
+		pspeed_lvl4: "Rapide",
+		pspeed_lvl5: "Éclair",
+		
+		pheight_title: "TAILLE PLANCHE",
+		pheight_lvl1: "Fou",
+		pheight_lvl2: "Petit",
+		pheight_lvl3: "Normal",
+		pheight_lvl4: "Grand",
+		pheight_lvl5: "Pourquoi ?",
+	
+		ballspeed_title: "VITESSE BALLE",
+		ballspeed_lvl1: "Ennuyeux",
+		ballspeed_lvl2: "Lent",
+		ballspeed_lvl3: "Normal",
+		ballspeed_lvl4: "Rapide",
+		ballspeed_lvl5: "Fulgurant",
+	
+		ballsize_title: "TAILLE BALLE",
+		ballsize_lvl1: "Quantique", 
+		ballsize_lvl2: "Petit",
+		ballsize_lvl3: "Normal",
+		ballsize_lvl4: "Grand",
+		ballsize_lvl5: "Cosmique",
+		player1: "Joueur 1",
+		player2: "Joueur 2",
+		player3: "Joueur 3",
+
+		card1 : "BIENVENUE DANS LE MUSÉE VIRTUEL DE PONG...",
+		card2 : "LA MACHINE PONG D’ATARI EST SORTIE EN 1972",
+		card3 : "C'ÉTAIT LE PREMIER<br>JEU VIDÉO D’ARCADE",
+		card4 : "À CONNAÎTRE UN<br>ÉNORME SUCCÈS COMMERCIAL...",
+		card5 : "LES 10 ANNÉES SUIVANTES SERAIENT CONNUES COMME",
+		card6 : "L’ÂGE D’OR DES<br>JEUX VIDÉO D’ARCADE",
+		card7 : "AVANT LE CRASH DU JEU VIDÉO DE 1983",
+		card8 : "QUAND LES CONSOLES<br>ET ORDINATEURS DOMESTIQUES",
+		card9 : "ONT PRIS LE RELAIS ET LES ARCADES SE SONT VIDÉES",
+		card10 : "ET LE PAUVRE PONG<br>N’A PLUS JAMAIS ÉTÉ REVU...",
+		card11 : "JUSQU’À AUJOURD’HUI...<br>CONNECTEZ-VOUS POUR JOUER !",
+
+		win: "Victoires"
+	},
+	
+	es: {	
+		flag : "🇪🇸",
+		homeTitle: "MUNDO DE PONG",
+		subTitle: "Edición Trascendencia",
+		modeTitle: "MODO DE JUEGO",
+
+		pspeed_title: "VELOCIDAD DE LA PALETA",
+		pspeed_lvl1: "Doloroso",
+		pspeed_lvl2: "Lento",
+		pspeed_lvl3: "Normal",
+		pspeed_lvl4: "Rápido",
+		pspeed_lvl5: "Relámpago",
+		
+		pheight_title: "ALTURA DE LA PALETA",
+		pheight_lvl1: "Loco",
+		pheight_lvl2: "Pequeño",
+		pheight_lvl3: "Normal",
+		pheight_lvl4: "Grande",
+		pheight_lvl5: "¿Por qué?",
+	
+		ballspeed_title: "VELOCIDAD DE LA PELOTA",
+		ballspeed_lvl1: "Aburrido",
+		ballspeed_lvl2: "Lento",
+		ballspeed_lvl3: "Normal",
+		ballspeed_lvl4: "Rápido",
+		ballspeed_lvl5: "Veloz",
+	
+		ballsize_title: "TAMAÑO DE LA PELOTA",
+		ballsize_lvl1: "Cuántico", 
+		ballsize_lvl2: "Pequeño",
+		ballsize_lvl3: "Normal",
+		ballsize_lvl4: "Grande",
+		ballsize_lvl5: "Cósmico",
+		player1: "Jugador 1",
+		player2: "Jugador 2",
+		player3: "Jugador 3",
+
+		card1: "BIENVENIDO AL MUSEO VIRTUAL DE PONG...",
+		card2: "LA MÁQUINA PONG DE ATARI FUE LANZADA EN 1972",
+		card3: "FUE EL PRIMER<br>VIDEOJUEGO DE ARCADE",
+		card4: "EN TENER UN<br>ENORME ÉXITO COMERCIAL...",
+		card5: "LOS PRÓXIMOS 10 AÑOS SERÍAN CONOCIDOS COMO",
+		card6: "LA EDAD DE ORO DE<br>LOS VIDEOJUEGOS DE ARCADE",
+		card7: "ANTES DEL CRASH DE LOS VIDEOJUEGOS DE 1983",
+		card8: "CUANDO LAS CONSOLAS<br>Y ORDENADORES DOMÉSTICOS",
+		card9: "TOMARON EL CONTROL Y LOS ARCADES SE VACÍARON",
+		card10: "Y EL POBRE PONG<br>DESAPARECIÓ PARA SIEMPRE...",
+		card11: "HASTA HOY...<br>¡INICIA SESIÓN PARA JUGAR!",
+
+		win: "Victorias"
+	}
+};
